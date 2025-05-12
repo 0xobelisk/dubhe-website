7b21f04d5173d05c13a1a8e22afb0b0ab6d16abd
vladilen11 <vladilenaksana@163.com>
2025-05-12 20:58:53 +0800

    update dubhe website info

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" suppressHydrationWarning className="dark">
+		<html lang="en" suppressHydrationWarning>
 			<head>
 				<link
 					rel="icon"
